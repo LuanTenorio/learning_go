43723223366cea017d5217e2d9719de351ae3d1b
Luan Tenório <lluantenorio7@gmail.com>
2024-12-30 23:07:52 -0300

    Add postgres connection

--- web/main.go
+++ web/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type postgresConfig struct {
+	url string
+}
 
 type Product struct {
 	Name        string
@@ -15,6 +25,11 @@ type Product struct {
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
+	loadEnv()
+
+	postgresUrl := os.Getenv("PG_URL")
+	db := connectDB(postgresUrl)
+	defer db.Close()
 
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
@@ -27,4 +42,21 @@ func index(w http.ResponseWriter, r *http.Request) {
 	}
 
 	temp.ExecuteTemplate(w, "Index", products)
+}
+
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Erro ao carregar o arquivo .env: ", err)
+		os.Exit(1)
+	}
+}
+
+func connectDB(url string) *sql.DB {
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
 }
